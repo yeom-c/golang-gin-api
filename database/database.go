8161c6ac0583b945dac37934a7354baf0fd1fa71
yeom-c <yjc720@gmail.com>
2023-09-05 18:00:50 +0900

    feat: gorm 쿼리 로그

--- database/database.go
+++ database/database.go
@@ -8,13 +8,16 @@ import (
 	"github.com/yeom-c/golang-gin-api/model"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var Conn *gorm.DB
 
 func init() {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", config.EnvVar.DBUser, config.EnvVar.DBPassword, config.EnvVar.DBHost, config.EnvVar.DBPort, config.EnvVar.DBName)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
 	if err != nil {
 		log.Fatal("failed to connect database: ", err)
 	}
